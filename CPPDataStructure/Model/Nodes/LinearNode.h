a0bb412d87b846f0f60e89b66fe1b9d672c30f5c
Alex Rottlaender <AlexRottlaender@gmail.com>
2019-01-30 12:37:15 -0700

    asdf

---
+++ CPPDataStructure/Model/Nodes/LinearNode.h
@@ -0,0 +1,57 @@
+//
+//  LinearNode.h
+//  CPPDataStructure
+//
+//  Created by Rottlaender, Alex on 1/30/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_h
+#define LinearNode_h
+
+#include "Node.hpp"
+
+template<class Type>
+class LinearNode : public Node<Type>
+{
+private:
+    LinearNode<Type> * next;
+public:
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    LinearNode<Type> * getNext();
+    void setNext(LinearNode<Type> * next);
+};
+
+//Implementation of templates
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node()
+{}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNext()
+{
+    return this->next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNext(LinearNode<Type> * next)
+{
+    this->next = next;
+}
+
+
+#endif /* LinearNode_h */
