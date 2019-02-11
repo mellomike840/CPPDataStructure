ebcc48c53e67e66a4373315e364e4764d992962a
Alex Rottlaender <AlexRottlaender@gmail.com>
2019-02-11 13:22:45 -0700

    asdf

--- CPPDataStructure/Model/Linear/LinkedList.hpp
+++ CPPDataStructure/Model/Linear/LinkedList.hpp
@@ -29,10 +29,10 @@ public:
     LinearNode<Type> * getEnd();
     
     //Structure Methods
-    void add(Type item);
-    void addAtIndex(int index, Type item);
-    Type getFromIndex(int index);
-    Type remove(int index);
+    virtual void add(Type item);
+    virtual void addAtIndex(int index, Type item);
+    virtual Type getFromIndex(int index);
+    virtual Type remove(int index);
     // Type setAtIndex(int index, Type item);
     // bool contains(Type item);
 };
@@ -131,7 +131,7 @@ Type LinkedList<Type> :: remove(int index)
 {
     assert(index >= 0 && index < this->size);
     
-    LinearNode<Type> * current = font;
+    LinearNode<Type> * current = front;
     LinearNode<Type> * toBeRemoved = nullptr;
     LinearNode<Type> * previous = nullptr;
     
@@ -191,16 +191,22 @@ int LinkedList<Type> :: getSize() const
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+template <class Type>
+bool LinkedList<Type> :: contains(Type thingToFind)
+{
+    bool exist = false;
+    
+    LinearNode<Type> * searchPoint = front;
+    
+    for(int index  = 0; index < getSize(); index++)
+    {
+        if(searchPointer->getData() == thingtoFind)
+        {
+            return true;
+        }
+        searchPointer = searchPointer ->getNextNode();
+    }
+    return exists;
+}
 
 #endif /* LinkedList_hpp */
---
+++ CPPDataStructure/Model/Linear/Stack.h
@@ -0,0 +1,136 @@
+//
+//  Stack.h
+//  CPPDataStructure
+//
+//  Created by Rottlaender, Alex on 2/11/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Stack_h
+#define Stack_h
+
+#include "LinkedList.hpp"
+
+template <class Type>
+class Stack : public LinkedList<Type>
+{
+public:
+    Stack();
+    ~Stack();
+    
+    //Stack specific methods
+    void push(Type data);
+    Type pop();
+    Type peek();
+    
+    //Overridden LinkedList methods
+    void add(Type data);
+    void addAtIndex(int Index, Type data);
+    Type getFromIndex(int Index);
+    Type remove(int index);
+}
+
+template <class Type>
+Stack<Type> :: Stack() : LinkedList<Type>()
+{
+    //Empty
+}
+
+template <class Type>
+Stack<Type> :: ~Stack()
+{
+    while(this-> size > 0)
+    {
+        pop();
+    }
+}
+
+
+template <class Type>
+void Stack<Type> :: push(Type data)
+{
+    LinearNode<Type> * add new LinearNode<Type>(data);
+    
+    if(this->size == 0)
+    {
+        this->end = add;
+    }
+    else
+    {
+        add->setNextNode(this->front);
+    }
+    
+    this->front = add;
+    this->size++;
+}
+
+template <class Type>
+void Stack<Type> :: add(Type data)
+{
+    push(data);
+}
+
+template <class Type>
+void Stack<Type> :: addAtIndex(int index, Type data)
+{
+    assert(index == 0);
+    push(data);
+}
+
+
+template <class Type>
+Type Stack<Type> :: pop()
+{
+    assert (this->size > 0);
+    Type removed = this->front->getData();
+    
+    LinearNode<Type> * removedNode = this->getFront();
+    this->front = removedNode->getNextNode();
+    delete removedNode;
+    
+    this->size--;
+    
+    return removed;
+}
+
+template <class Type>
+Type stack<Type> :: remove(int index)
+{
+    assert(index == 0);
+    return pop();
+}
+
+Type Stack<Type> :: peek()
+{
+    assert(this->size > 0);
+    return this->getFront()->getData();
+}
+
+template <class Type>
+Type Stack<Type> :: getFromIndex(int index)
+{
+    assert(index == 0);
+    return peek();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* Stack_h */
