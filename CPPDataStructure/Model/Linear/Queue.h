4cecbc328dec08d7be4be66c4415e0f09e3e166a
Alex Rottlaender <AlexRottlaender@gmail.com>
2019-02-13 12:32:25 -0700

    asdf

--- CPPDataStructure/Model/Linear/Queue.h
+++ CPPDataStructure/Model/Linear/Queue.h
@@ -47,4 +47,77 @@ Queue<Type> :: ~Queue()
 }
 
 
+template <class Type>
+void Queue<Type> :: enqueue(Type item)
+{
+    LinearNode<Type> * added = new LinearNode<Type>(item);
+    
+    if(this->size == 0)
+    {
+        this->front = added;
+    }
+    else
+    {
+        this->end->setNextNode(added);
+    }
+    
+    this->end = added;
+    this->size++;
+}
+}
+
+template <class Type>
+void Queue<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index == this->size);
+    enqueue(item);
+}
+
+template <class Type>
+Type Queue<Type> :: dequeue()
+{
+    assert(this->size > 0);
+    
+    Type returned = this->front->getData();
+    
+    LinearNode<Type> * removed = this->front;
+    this->front = removed->getNextNode();
+    
+    delete removed;
+    
+    this->size = this->size - 1;
+    
+    return returned;
+}
+
+template <class Type>
+Type Queue<Type> :: remove(int index)
+{
+    assert(index == 0);
+    return dequeue();
+}
+
+template <class Type>
+void Queue<Type> :: clear()
+{
+    while(this->front != nullptr)
+    {
+        cout <, dequeue() << endl;
+    }
+}
+
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->size > 0);
+    return this->getFront()->getData();
+}
+
+template <class Type>
+Type Queue<Type> :: getFromIndex(int index)
+{
+    assert(index == 0);
+    return peek();
+}
+
 #endif /* Queue_h */
